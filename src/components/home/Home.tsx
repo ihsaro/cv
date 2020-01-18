acf78651ad4838fa6e5e67fa09566f9d0da06b50
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-01-18 16:45:02 +0000

    personal details completed

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import TitleBox from '../shared/TitleBox/TitleBox';
+import PersonalDetailItem from './PersonalDetailItem';
+
 import '../shared/All.css';
 
 export default class Home extends React.Component {
@@ -14,6 +16,11 @@ export default class Home extends React.Component {
                         I am a professional software developer seeking out new challenges and opportunities to grow my career. I
                         am an open-source enthusiast who works with linux as my daily driver and I my motivation to work is to
                         make the world a place where technology ...
+                    </div>
+
+                    <div className="container shadow p-3 mb-5 bg-white rounded content-box-container-width">
+                        <h5 className="content-box-header">PERSONAL DETAILS</h5>
+                        <PersonalDetailItem jsonResource={require('./json/PersonalDetailItem.json')} />
                     </div>
                 </div>
             </div>
---
+++ src/components/home/PersonalDetailItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IPersonalDetailItemProps } from './interfaces/IPersonalDetailItemProps';
+
+export default class PersonalDetailItem extends React.Component<IPersonalDetailItemProps> {
+    render() {
+        return (
+            this.props.jsonResource.PersonalDetailItems.map((item: any) => {
+                return(
+                    <div className="row">
+                        <div className="col-md-2"><strong>{item.title}</strong></div>
+                        <div className="col-md-10">{item.content}</div>
+                    </div>
+                )
+            })
+        );
+    }
+}
