6f082641a1e4ca6147fef7b59accad1fe6e09f22
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-01-18 19:17:01 +0000

    home bac removed

--- src/components/home-bac/interfaces/IPersonalDetailItemProps.ts
+++
@@ -1,3 +0,0 @@
-export interface IPersonalDetailItemProps {
-    jsonResource: any;
-}
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,7 +8,7 @@ import '../shared/All.css';
 export default class Home extends React.Component {
     render() {
         return (
-            <div className="row section-top-padding">
+            <div id="home" className="row section-top-padding">
                 <TitleBox title="Home" fontAwesomeIcon="fa fa-home fa-5x" />
                 <div className="col-md-10">
                     <div className="container shadow p-3 mb-5 bg-white rounded content-box-container-width">
