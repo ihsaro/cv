6345156bb7dce477ebc4ff892c62a640a670da2f
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-02-01 10:15:29 +0000

    education section finalized

--- src/components/education/EducationCard.tsx
+++ src/components/education/EducationCard.tsx
@@ -9,14 +9,32 @@ export default class EducationCard extends React.Component<IEducationProps> {
             <div className="row">
             {
                 this.props.jsonResource.EducationCardItems.map((item: any, index: number) => {
-                    return(
-                        <div className="col-sm-4">
+                    return (
+                        <div className="col-sm-6">
                             <div className="card education-card">
                                 <img className="education-card-photo" src={this.props.cardPictures[index]} alt="..."/>
                                 <div className="card-body">
-                                    <strong className="card-title line-breaker">{item.title}</strong>
-                                    <label className="line-breaker">Location: {item.location}</label>
-                                    <label>Year: {item.year}</label>
+                                    <div className="text-center">
+                                        <strong className="text-center card-title line-breaker">{item.title}</strong>
+                                    </div>
+                                    <label className="line-breaker"><strong>Location:</strong> {item.location}</label>
+                                    <label className="line-breaker"><strong>Year:</strong> {item.year}</label>
+                                    <table className="maximize-width">
+                                        <tr>
+                                            <th>Subject</th>
+                                            <th>Result</th>
+                                        </tr>
+                                    {
+                                        item.results.map((result: any) => {
+                                            return (
+                                                <tr>
+                                                    <td>{result.subject}</td>
+                                                    <td>{result.result}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                    </table>
                                 </div>
                             </div>
                         </div>
