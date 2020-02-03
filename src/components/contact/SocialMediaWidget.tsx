b1290905e5fa4d7c5fc6f6ebbf6a1e5b8aab8a5b
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-02-03 18:42:36 +0000

    website beta ready

--- src/components/contact/SocialMediaWidget.tsx
+++ src/components/contact/SocialMediaWidget.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ISocialMediaWidgetProps } from './interfaces/ISocialMediaWidgetProps';
 
+import './SocialMediaWidget.css';
+
 export default class SocialMediaWidget extends React.Component<ISocialMediaWidgetProps> {
     render() {
         return (
             this.props.resource.SocialMediaWidgetItems.map((item: any, index: number) => {
                 return (
-                    <div className="col-md-3">
-                        <i className={item.icon}></i>
-                    </div>
+                    <a href={item.url} target="_blank"><i className={`${item.icon} social-media-icon`}></i></a>
                 )
             })
         );
